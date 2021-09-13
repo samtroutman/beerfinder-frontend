a75a13856c6945a9b50ee98919aea824662baa49
samtroutman <troutm16@gmail.com>
2021-09-12 22:27:48 -0400

    add render card

--- src/components/Category.js
+++ src/components/Category.js
@@ -8,20 +8,26 @@ class Category {
         console.log(this)
     }
 
-    static addCategory(category){
-        new Category(category)
+    renderCard = () => {
+        const {name, imageURL} = this.data
+        document.querySelector(".category-container").innerHTML +=
+        `<div class="category-card">
+            <img src=${imageURL} alt=${name}/>
+            <p>${name}</p>
+        </div>`
     }
 
     static renderIndex(){
         const categoryContainer = document.createElement("div")
         categoryContainer.classList.add("category-container")
         document.getElementById("main").appendChild(categoryContainer)
+        this.all.forEach(category => category.renderCard())
     }
 
 
     static getCategories(){
         api.getCategories().then(categories => {
-            categories.forEach(category => Category.addCategory(category))
+            categories.forEach(category => new Category(category))
             this.renderIndex()
         })
     }
