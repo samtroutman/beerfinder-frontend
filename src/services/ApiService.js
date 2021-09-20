e9fb3740b2ff8c8945bcf73eb943ed481fee8f9f
samtroutman <troutm16@gmail.com>
2021-09-19 20:23:32 -0400

    remove bad code

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -5,13 +5,4 @@ class ApiService {
 
     getCategories = () => fetch(this.api + "/categories").then(res => res.json())
 
-    likeBeer = (id, likes) => fetch(`${this.api}/beers/${id}`, {
-        method: 'PATCH', 
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({likes: likes}),
-      })
-      .then(res => res.json())
-
 }
