d508ccc313311bd25715b36313eb6bfc31d54cff
samtroutman <troutm16@gmail.com>
2021-09-20 22:46:27 -0400

    add handle like static

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -15,10 +15,23 @@ class Beer {
         <h2>IBU: ${ibu} ABV: ${abv}</h2>
         <p>${description}</p>
         <p>Likes: ${likes}</p>
-        <button class="like-button">Like ${name}</button>
-        </div>`  
+        <button class="like-button" id="like-btn">Like ${name}</button>
+        </div>`
+        document.getElementById("like-btn").addEventListener("click", Beer.handleLike)
 
     }
+
+    static handleLike = (e) => {
+        document.querySelectorAll(".like-button").forEach(function(e){
+            e.addEventListener("click", Beer.handleLike)
+      })
+        if (e.target.classList.contains("like-button")) {
+            const beerCard = e.target.closest(".beer-card")
+            const id = beerCard.dataset.id
+            console.log(id)
+        }
+    }
+    
 
     get beerContainer() {
         return document.querySelector(".beer-container")
